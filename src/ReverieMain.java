e774b2e267796750156efb8ed171c9c17e409167
isko-algorithm <gjpgagno@gmail.com>
2014-11-09 20:34:44 +0800

    added load/save user settings

--- src/ReverieMain.java
+++ src/ReverieMain.java
@@ -8,6 +8,7 @@ import reverie.view.ReverieMainFrame;
 public class ReverieMain {
     public static void main(String args[]){
         AppState.initalizeState();
+        AppState.setState("Gabby","titi","month");
         //ReverieMainFrame mainFrame = new ReverieMainFrame();
     }
 }
--- src/reverie/scheduler/AppState.java
+++ src/reverie/scheduler/AppState.java
@@ -1,7 +1,10 @@
 package reverie.scheduler;
 
 import reverie.model.*;
+
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Properties;
 
 /**
  * Created by Dell on 11/9/2014.
@@ -10,9 +13,50 @@ public abstract class AppState {
     private static ArrayList<Job> schedule;
     private static ArrayList<Task> priorityQueue;
     private static ArrayList<Habit> habitQueue;
+    private static boolean firstRun;
+    private static String user;
+    private static String preferredPrio;
+    private static String preferredView;
 
     public static void initalizeState(){
+        String propFile = "resources/config.properties";
+        Properties prop = new Properties();
+        InputStream io = AppState.class.getClassLoader().getResourceAsStream(propFile);
+        try{
+            prop.load(io);
+            firstRun = Boolean.valueOf(prop.getProperty("firstrun"));
+            user = prop.getProperty("user");
+            preferredPrio = prop.getProperty("preferredprio");
+            preferredView = prop.getProperty("preferredview");
+            System.out.println("Settings File loaded");
+        }
+        catch (IOException ex){
+            System.out.println("No properties file! contact developer gjpgagno@uplb.edu.ph");
+        }
+    }
 
+    public static void setState(String user, String preferredPrio, String preferredView){
+        String propFile = "src/resources/config.properties";
+        Properties prop = new Properties();
+        try{
+            OutputStream out = new FileOutputStream(propFile);
+            prop.setProperty("firstrun", "false");
+            prop.setProperty("user", user);
+            prop.setProperty("preferredprio", preferredPrio);
+            prop.setProperty("preferredview", preferredView);
+
+            prop.store(out, null);
+            out.close();
+
+            AppState.user = user;
+            AppState.preferredPrio = preferredPrio;
+            AppState.preferredView = preferredView;
+            AppState.firstRun = false;
+
+        }
+        catch(IOException ex){
+            System.out.println("Failed to Write! contact developer gjpgagno@uplb.edu.ph");
+        }
     }
 
     //get methods
