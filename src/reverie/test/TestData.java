5da699380bf0109e2dc2677d29aac9a0356df503
isko-algorithm <gjpgagno@gmail.com>
2014-11-21 06:45:46 +0800

    Devtest: read input files

--- src/reverie/test/TestData.java
+++ src/reverie/test/TestData.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 /**
  * Created by Dell on 11/9/2014.
@@ -13,6 +14,12 @@ public abstract class TestData {
         String fileDir = "src/resources/testinputs";
         try {
             Scanner readLine = new Scanner(new File(fileDir));
+            while(readLine.hasNext()){
+                StringTokenizer tok = new StringTokenizer(readLine.nextLine(), " ");
+                while(tok.hasMoreTokens()){
+                    System.out.println(tok.nextToken());
+                }
+            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch(IOException e){
